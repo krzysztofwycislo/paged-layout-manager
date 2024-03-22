e18cb199f466e3138a5f8ca1c95f804d9a07cbde
Krzysztof Wycisło <krzysztofwycislo@gmail.com>
2024-03-22 22:29:44 +0100

    Refactored to use recycling process, added rtl handling

--- app/src/main/java/com/handsome/club/paged_layout_manager/ui/component/PagedHorizontalLayoutManager.kt
+++ app/src/main/java/com/handsome/club/paged_layout_manager/ui/component/PagedHorizontalLayoutManager.kt
@@ -1,5 +1,8 @@
 package com.handsome.club.paged_layout_manager.ui.component
 
+import android.graphics.Rect
+import android.view.View
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.RecyclerView
 import timber.log.Timber
 import kotlin.math.max
@@ -14,53 +17,94 @@ class PagedHorizontalLayoutManager(
 
     private val itemsInPage = size.columns * size.rows
 
-    private var offset = 0
+    private var scrollOffset = 0
 
+    private var itemRects = arrayListOf<Rect>()
+
+    private val itemWidth get() = width / size.columns
+    private val itemHeight get() = height / size.rows
+
+    private val recyclerViewRect
+        get() = Rect().apply {
+            left = scrollOffset
+            top = 0
+            right = left + width
+            bottom = height
+        }
 
     override fun onLayoutChildren(
         recycler: RecyclerView.Recycler,
         state: RecyclerView.State?
     ) {
-        fill(recycler)
+        if (itemCount == 0) return
+
+        itemRects.clear()
+
+        calculateAllItemsPositions()
+            .also(itemRects::addAll)
+
+        fillGrid(recycler)
     }
 
-    private fun fill(recycler: RecyclerView.Recycler) {
-        if (itemCount == 0) return
-        detachAndScrapAttachedViews(recycler)
-
-        val itemWidth = width / size.columns
-        val itemHeight = height / size.rows
-
-        for (position in 0 until itemCount) {
+    private fun calculateAllItemsPositions(): List<Rect> {
+        return (0 until itemCount).map { position ->
             val itemPage = position / itemsInPage
             val itemRow = (position % itemsInPage / size.columns)
 
-            Timber.d("onLayoutChildren position = $position page = $itemPage row = $itemRow")
+            val (left, right) = if (isLayoutRTL()) {
+                val right = ((position % size.columns) * -itemWidth + itemPage * -width) + width
+                val left = right - itemWidth
 
-            val view = recycler.getViewForPosition(position)
-                .also(::addView)
+                left to right
+            } else {
+                val left = ((position % size.columns) * itemWidth + itemPage * width)
+                val right = left + itemWidth
 
-            view.layoutParams = (view.layoutParams as RecyclerView.LayoutParams)
-                .apply {
-                    width = itemWidth
-                    height = itemHeight
-                }
-
-            val left = ((position % size.columns) * itemWidth + itemPage * width) - offset
-            val right = left + itemWidth
+                left to right
+            }
 
             val top = itemRow * itemHeight
             val bottom = top + itemHeight
 
-            Timber.d("onLayoutChildren left = $left right = $right top = $top bottom = $bottom")
-
-            measureChild(view, itemWidth, itemHeight)
-            layoutDecorated(view, left, top, right, bottom)
+            Rect(left, top, right, bottom)
         }
+    }
 
-        recycler.scrapList.forEach {
+    private fun fillGrid(recycler: RecyclerView.Recycler) {
+        detachAndScrapAttachedViews(recycler)
+
+        getVisibleViews(recycler)
+            .forEach { (view, itemRect) ->
+                view.layoutParams = (view.layoutParams as RecyclerView.LayoutParams)
+                    .apply {
+                        width = itemWidth
+                        height = itemHeight
+                    }
+
+                addView(view)
+                measureChildWithMargins(view, itemWidth, itemHeight)
+                layoutDecoratedWithMargins(
+                    view,
+                    itemRect.left - scrollOffset,
+                    itemRect.top,
+                    itemRect.right - scrollOffset,
+                    itemRect.bottom
+                )
+            }
+
+        val scrapListCopy = recycler.scrapList.toList()
+        scrapListCopy.forEach {
             recycler.recycleView(it.itemView)
         }
+    }
+
+    private fun getVisibleViews(recycler: RecyclerView.Recycler): List<Pair<View, Rect>> {
+        return itemRects.mapIndexed { index, itemRect ->
+            if (Rect.intersects(recyclerViewRect, itemRect))
+                recycler.getViewForPosition(index) to itemRect
+            else
+                null
+        }.filterNotNull()
     }
 
     override fun scrollHorizontallyBy(
@@ -73,15 +117,23 @@ class PagedHorizontalLayoutManager(
         val pages = itemCount / itemsInPage
 
         val fullSize = pages * width
-        val lastOffset = offset
+        val lastOffset = scrollOffset
 
-        offset = min(max(0, offset + dx), fullSize)
-        fill(recycler)
-        return lastOffset - offset
+        scrollOffset = if (isLayoutRTL())
+            min(max(-fullSize, scrollOffset + dx), 0)
+        else
+            min(max(0, scrollOffset + dx), fullSize)
+
+        fillGrid(recycler)
+        return lastOffset - scrollOffset
     }
 
     override fun canScrollHorizontally(): Boolean {
         return true
+    }
+
+    private fun isLayoutRTL(): Boolean {
+        return layoutDirection == ViewCompat.LAYOUT_DIRECTION_RTL
     }
 
     override fun generateDefaultLayoutParams(): RecyclerView.LayoutParams =
@@ -90,4 +142,10 @@ class PagedHorizontalLayoutManager(
             RecyclerView.LayoutParams.WRAP_CONTENT
         )
 
+}
+
+
+// TODO remove
+private fun Rect.toBetterString(): String? {
+    return "Rect(left = $left, right = $right, top = $top, bottom = $bottom)"
 }
