9dba9e6609ad9794993c0d0d93405932cdf52492
Krzysztof Wycisło <krzysztofwycislo@gmail.com>
2024-03-22 16:02:55 +0100

    Added scrolling to layout manager

--- app/src/main/java/com/handsome/club/paged_layout_manager/ui/component/PagedHorizontalLayoutManager.kt
+++ app/src/main/java/com/handsome/club/paged_layout_manager/ui/component/PagedHorizontalLayoutManager.kt
@@ -2,6 +2,8 @@ package com.handsome.club.paged_layout_manager.ui.component
 
 import androidx.recyclerview.widget.RecyclerView
 import timber.log.Timber
+import kotlin.math.max
+import kotlin.math.min
 
 private val Pair<Int, Int>.columns get() = this.first
 private val Pair<Int, Int>.rows get() = this.second
@@ -12,12 +14,19 @@ class PagedHorizontalLayoutManager(
 
     private val itemsInPage = size.columns * size.rows
 
+    private var offset = 0
+
 
     override fun onLayoutChildren(
         recycler: RecyclerView.Recycler,
         state: RecyclerView.State?
     ) {
+        fill(recycler)
+    }
+
+    private fun fill(recycler: RecyclerView.Recycler) {
         if (itemCount == 0) return
+        detachAndScrapAttachedViews(recycler)
 
         val itemWidth = width / size.columns
         val itemHeight = height / size.rows
@@ -37,7 +46,7 @@ class PagedHorizontalLayoutManager(
                     height = itemHeight
                 }
 
-            val left = ((position % size.columns) * itemWidth + itemPage * width)
+            val left = ((position % size.columns) * itemWidth + itemPage * width) - offset
             val right = left + itemWidth
 
             val top = itemRow * itemHeight
@@ -48,6 +57,27 @@ class PagedHorizontalLayoutManager(
             measureChild(view, itemWidth, itemHeight)
             layoutDecorated(view, left, top, right, bottom)
         }
+
+        recycler.scrapList.forEach {
+            recycler.recycleView(it.itemView)
+        }
+    }
+
+    override fun scrollHorizontallyBy(
+        dx: Int,
+        recycler: RecyclerView.Recycler,
+        state: RecyclerView.State?
+    ): Int {
+        if (itemCount == 0) return 0
+
+        val pages = itemCount / itemsInPage
+
+        val fullSize = pages * width
+        val lastOffset = offset
+
+        offset = min(max(0, offset + dx), fullSize)
+        fill(recycler)
+        return lastOffset - offset
     }
 
     override fun canScrollHorizontally(): Boolean {
